eaaa7e2acd96b538c1f18938917727d6ee1616a5
rockingkp <kaamyakpant@gmail.com>
2024-01-14 02:09:29 +0530

    update: added navlinks

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
+const links = [
+  { href: "/chat", label: "Chat" },
+  { href: "/tours", label: "Tours" },
+  { href: "/tours/new-tour", label: "New Tour" },
+  { href: "/profile", label: "Profile" },
+];
+
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  return (
+    <ul className="menu text-base-content">
+      {links.map((link) => {
+        return (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavLinks;
