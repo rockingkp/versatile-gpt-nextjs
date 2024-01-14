42816fbb4d50fad5310760ced41c8c89a4f51956
rockingkp <kaamyakpant@gmail.com>
2024-01-14 22:07:13 +0530

    update: added context to the messages

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,18 +3,32 @@
 import { generateChatResponse } from "@/utils/action";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Chat = () => {
   const [text, setText] = useState("");
-  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
 
   const { mutate } = useMutation({
-    mutationFn: (message) => generateChatResponse(message),
+    mutationFn: (query) => generateChatResponse([...messages, query]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("Something went wrong..");
+        return;
+      }
+
+      setMessages((prev) => [...prev, data]);
+    },
   });
+
+  console.log(messages);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    mutate(text);
+    const query = { role: "user", content: text };
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText("");
   };
 
   return (
