f461bd5da7ba7a134712bcac4a14c7632cabeb6a
rockingkp <kaamyakpant@gmail.com>
2024-01-15 17:05:10 +0530

    update: new tour form added

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import TourInfo from "./TourInfo";
 
 const NewTour = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log(destination);
+  };
   return (
-    <div>NewTour</div>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit} className="max-w-2xl">
+        <h2 className="mb-4">Select your dream destination</h2>
+        <div className="join w-full">
+          <input
+            type="text"
+            className="join-item w-full input input-bordered"
+            placeholder="City"
+            name="city"
+            required
+          />
+          <input
+            type="text"
+            className="join-item w-full input input-bordered"
+            placeholder="Country"
+            name="country"
+            required
+          />
+          <button className="btn btn-primary join-item" type="submit">
+            Generate Tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">
+        <TourInfo />
+      </div>
+    </>
+  );
+};
 
-export default NewTour
+export default NewTour;
