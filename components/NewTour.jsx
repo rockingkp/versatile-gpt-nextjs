23ab4fe8b6ebd53b2e5d205358bec1aa2b165bb9
rockingkp <kaamyakpant@gmail.com>
2024-01-17 02:21:17 +0530

    update: setting up react query for new tour

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -2,14 +2,36 @@
 
 import React from "react";
 import TourInfo from "./TourInfo";
+import { useMutation } from "@tanstack/react-query";
+import { generateTourResponse } from "@/utils/action";
 
 const NewTour = () => {
+  const {
+    mutate,
+    isPending,
+    data: tour,
+  } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination);
+      if (newTour) {
+        return newTour;
+      }
+      toast.error("No matching city found...");
+      return null;
+    },
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const destination = Object.fromEntries(formData.entries());
-    console.log(destination);
+    mutate(destination);
   };
+
+  if (isPending) {
+    return <span className="loading loading-lg"></span>;
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="max-w-2xl">
@@ -34,9 +56,7 @@ const NewTour = () => {
           </button>
         </div>
       </form>
-      <div className="mt-16">
-        <TourInfo />
-      </div>
+      <div className="mt-16">{tour ? <TourInfo tour={tour} /> : null}</div>
     </>
   );
 };
