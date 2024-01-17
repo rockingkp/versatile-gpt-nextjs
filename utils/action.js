a01b9784b895f99642206c2593781dddb80fa8db
rockingkp <kaamyakpant@gmail.com>
2024-01-18 02:23:44 +0530

    update: single tour page added

--- utils/action.js
+++ utils/action.js
@@ -111,3 +111,11 @@ export const getAllTours = async (searchTerm) => {
   });
   return tours;
 };
+
+export const getSingleTour = async (id) => {
+  return prisma.tour.findUnique({
+    where: {
+      id,
+    },
+  });
+};
